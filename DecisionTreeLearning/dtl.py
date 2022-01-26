618f4d76d670e4bc19406bcdb29320754f1b74c8
christopherchandrasaputra <13519074@std.stei.itb.ac.id>
2022-01-26 09:37:07 +0700

    Add DTL class

--- DecisionTreeLearning/dtl.py
+++ DecisionTreeLearning/dtl.py
@@ -1,5 +1,22 @@
+from typing import Any
+from DecisionTreeLearning.classes.dataset import Dataset
 import tools
 
 class DTL():
+    dataset:Dataset = None
+    tree:dict[Any,Any] = None
+
+    def __init__(self,dir:str):
+        self.dataset = Dataset(dir)
     
-    pass
+    def generate(self) -> dict[Any,Any]:
+        dataset_copy = self.dataset.copy()
+
+        # Find max(gain)
+
+        # Find field value correlation with labels
+
+        # Return result
+
+
+
