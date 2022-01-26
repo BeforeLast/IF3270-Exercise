b587dd89db2d97cc3be533c37207aa00629bbad3
christopherchandrasaputra <13519074@std.stei.itb.ac.id>
2022-01-26 09:36:09 +0700

    Add typehint, setter, copy, separate function Fix class constructor

--- DecisionTreeLearning/classes/dataset.py
+++ DecisionTreeLearning/classes/dataset.py
@@ -1,37 +1,59 @@
+# Type Hint
+from __future__ import annotations
+from typing import Any
+
 import csv
-from typing import Any, List
+
 class Dataset:
-    def __init__(self, dir:str):
+    data: list[list[Any]] = None
+    field: dict[Any,int] = None
+    field_values: dict[Any,list[Any]] = None
+
+    def __init__(self, dir:str=None):
         self.data = []
         self.field = {}
-        self.field_values = {}
-        with open(dir,'r',newline='',encoding='utf-8-sig') as file:
-            reader = csv.reader(file)
-            # Create temporary array
-            temp = []
-            for temp_v in reader:
-                temp.append(temp_v)
+        if dir is not None:
+            with open(dir,'r',newline='',encoding='utf-8-sig') as file:
+                reader = csv.reader(file)
+                # Create temporary array
+                temp = []
+                for temp_v in reader:
+                    temp.append(temp_v)
 
-            # Splitting field name with data
-            for i in range(len(temp[0])):
-                self.field[temp[0][i]] = i
-                self.field_values[temp[0][i]] = []
-                for j in range(1,len(temp)):
-                    if temp[j][i] not in self.field_values[temp[0][i]]:
-                        self.field_values[temp[0][i]].append(temp[j][i])
-            for row in temp[1:]:
-                self.data.append(row)
+                # Mapping field index
+                for i in range(len(temp[0])):
+                    self.field[temp[0][i]] = i
+                
+                # Splitting data
+                for row in temp[1:]:
+                    self.data.append(row)
+                
+                # Generating field list values
+                self.generate_fields_values()
+
+    def generate_fields_values(self):
+        self.field_values = {}
+        temp = self.data.copy()
+        data_value = [[temp[j][i] for j in range(len(temp))] for i in range(len(temp[0]))]
+        for key in self.field:
+            self.field_values[key] = list(set(data_value[self.field[key]]))
+
+    def set_fields(self,field:dict):
+        self.field = field
+
+    def set_data(self,data:list):
+        self.data = data
 
     def get_fields(self):
-        return self.field
+        return self.field.copy()
 
     def get_data(self):
-        return self.data
+        return self.data.copy()
 
-    def get_field_values(self, fieldname:str) -> List:
-        return self.field_values[fieldname]
+    def get_field_values(self, fieldname:str) -> list:
+        return self.field_values[fieldname].copy()
     
-    def get_specified_data(self,fieldname:str,value:Any) -> List:
+    def get_specified_data(self,fieldname:str,value:Any) -> list:
         if fieldname is None or value is None:
             raise ValueError("fieldname or/and value required!")
         if fieldname and value:
@@ -41,3 +63,32 @@ class Dataset:
                 if record[field_idx] == value:
                     temp.append(record)
             return temp
+    
+    def separate_by_value(self,fieldname:str,value:Any) -> Dataset:
+        data_copy = self.get_data()
+        temp_data = []
+
+        for record in data_copy:
+            if record[self.field[fieldname]] == value:
+                temp_data.append(record)
+        
+        return_dataset = Dataset()
+        return_dataset.set_data(temp_data)
+        return_dataset.set_fields(self.get_fields())
+        return_dataset.generate_fields_values()
+
+        return return_dataset
+    
+    def copy(self):
+        return_dataset = Dataset()
+        return_dataset.set_data(self.get_data())
+        return_dataset.set_fields(self.get_fields())
+        return_dataset.generate_fields_values()
+        return return_dataset
+
+if __name__=='__main__':
+    test = Dataset('./DecisionTreeLearning/Data/ex1.csv')
+    split_test = test.separate_by_value('PlayTennis','No')
+    print(split_test.get_data())
+    split_test_2 = split_test.separate_by_value('Outlook','Rain')
+    print(split_test_2.get_data())
